78ecaa45ca885ef687e2263be940d1d63d2ee51c
blin-1 <y.monsoon@gmail.com>
2018-02-04 21:49:49 -0500

    Entity Cleanup

--- web/app/model/Entity.js
+++ web/app/model/Entity.js
@@ -7,35 +7,5 @@ Ext.define('KeanBooks.model.Entity', {
     ],
     
     idProperty : 'rowId'
-    
-/*    proxy: {
-        type: 'rest',
-        url : 'http://localhost:8888/books/',
-        //url: 'https://kean-college-router.appspot.com/books/',
-        reader : {
-            rootProperty : 'rows'
-        }
-    }*/
-    
- // do not delete - mind not supported PUT on some servers
- /*   proxy : {
-        type : 'ajax',
-       
-        api : {
-            read :   'http://localhost:8888' + '/select',
-            create : 'http://localhost:8888' + '/create',
-            update : 'http://localhost:8888/update',
-            destroy :'http://localhost:8888/destroy'
-        },
-        reader : {
-            type : 'json',
-            rootProperty : 'rows'
-        },
-        writer : {
-            type : 'json',
-            rootProperty : 'data'//,
-            allowSingle : false
-        }
-    }*/
 
 });
